56104297986f19429d2ee968a9ec62e86f789bd7
nocturnalking <harsh.nocturnalking@gmail.com>
2021-01-24 23:36:30 -0600

    Remove Serialize

--- 2D_Platformer/Assets/Scripts/PlayerController.cs
+++ 2D_Platformer/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
     private Rigidbody2D rigid;
     // Start is called before the first frame update
     void Start()
