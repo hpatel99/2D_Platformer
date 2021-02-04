8791156ee085b205a06cd477d5d1545cf5f279d0
nocturnalking <harsh.nocturnalking@gmail.com>
2021-02-04 00:26:12 -0600

    dont know. just commit

--- 2D_Platformer/Assets/Scripts/PlayerController.cs
+++ 2D_Platformer/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -16,6 +17,7 @@ public class PlayerController : MonoBehaviour
     private Animator anim;
     private State CurrState;
     private float RunSpeed = 5;
+    private float hDirectionalinput;
 
     // Start is called before the first frame update
     void Start()
@@ -27,23 +29,28 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        float hDirection = Input.GetAxis("Horizontal");
+        //Update the horizontal input
+        hDirectionalinput = Input.GetAxis("Horizontal");
 
-        if (hDirection != 0f)
-        {
-            rigid.velocity = new Vector2(hDirection*RunSpeed, rigid.velocity.y);
-            transform.localScale = new Vector2(-hDirection >0 ? -1f : 1f, 1f);
-            anim.SetInteger("State", 1);
-        }
-        else
-        {
-            anim.SetBool("running", false);
-        }
+        // Update the Movement velocity. Do i want to call update every frame????
+        UpdateRigidbodyVelocity(hDirectionalinput);
 
         if (Input.GetKey(KeyCode.Space))
         {
             rigid.velocity = new Vector2(rigid.velocity.x, 3f) ;
+    }
 
+    private void UpdateRigidbodyVelocity(float hDirectionalinput)
+    {
+        if (hDirectionalinput != 0f)
+        {
+            rigid.velocity = new Vector2(hDirectionalinput * RunSpeed, rigid.velocity.y);
+            transform.localScale = new Vector2(-hDirectionalinput > 0 ? -1f : 1f, 1f);
+            anim.SetInteger("State", 1);
+        }
+        else
+        {
+            anim.SetInteger("State", 0);
         }
     }
 }
